96ced43c33bce4ffff9d4cde06ea4c561d7c4878
RainbowMango <renhongcai@huawei.com>
2020-08-08 16:41:55 +0800

    move tools.go out

--- tools.go
+++ tools.go
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package hack
+package tools
 
 import (
 	_ "github.com/bazelbuild/bazel-gazelle/cmd/gazelle"
